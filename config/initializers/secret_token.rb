bfb0f2d9c6731d1370d52890400da14e0852e204
ma <maxmyers@maxs-MacBook-Pro.local>
2013-06-18 16:41:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foldedears::Application.config.secret_key_base = '338778c35cc62f9a0566519855afc336b8e3486ad291f61701d7bd770f2ffed2da92ecbfc0e78d47ba3e75a96784802cb6605e4daa3f6ccb22ae42a569fa57a3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foldedears::Application.config.session_store :cookie_store, key: '_foldedears_session'
